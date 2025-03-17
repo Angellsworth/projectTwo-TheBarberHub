7cf1c3d0d1edfff0e54f835a7d79ccb52b96a139
Angellsworth <angellsworth@msn.com>
2025-03-17 13:18:05 -0600

    wip: set up profile route and controller

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
-
+const profileController = require('./controllers/profile.js')
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -28,12 +30,19 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+app.use(express.static('public'))//static asset middleware- used to send static assets to the client (css, images, dom manipulation javascript)
 
+// passUserToView middleware must come after session middleware
+app.use(passUserToView);
+
+app.get('/', (req, res) => {
+  if (req.session.user) {
+    res.redirect(`/users/${req.session.user._id}/profile`)
+  } else {
+    res.render('index.ejs')
+  }
+});
+//====== Turn vip lounge into Community page
 app.get('/vip-lounge', (req, res) => {
   if (req.session.user) {
     res.send(`Welcome to the party ${req.session.user.username}.`);
@@ -42,8 +51,12 @@ app.get('/vip-lounge', (req, res) => {
   }
 });
 
-app.use('/auth', authController);
+app.use('/auth', authController); // Users can access auth routes freely
+app.use(isSignedIn); // Everything below this now requires authentication
+
+// Protected profile page route
+app.use('/users/:userId/profile', profileController); // Users must be signed in to view/edit profile
 
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+  console.log(`ANGELA PANGELA! Your express app is FADING into view on port ${port}!`);
 });
