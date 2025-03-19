4b70b99a9c804d27bbcf9ecdf3c23db58d712b65
Angellsworth <angellsworth@msn.com>
2025-03-19 17:12:07 -0600

    wip: improved UI interactions—things should feel smoother now

--- public/js/script.js
+++ public/js/script.js
@@ -46,25 +46,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-// // Image preview before upload
-// document.addEventListener("DOMContentLoaded", function () {
-//     const fileInput = document.getElementById("profilePhotoInput");
-//     const previewImage = document.getElementById("profilePhotoPreview");
-
-//     if (fileInput && previewImage) {
-//         fileInput.addEventListener("change", function (event) {
-//             const file = event.target.files[0];
-//             if (file) {
-//                 const reader = new FileReader();
-//                 reader.onload = function (e) {
-//                     previewImage.src = e.target.result;
-//                 };
-//                 reader.readAsDataURL(file);
-//             }
-//         });
-//     }
-// });
-
 
 
 
