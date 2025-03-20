9cbc791f1c3f4e55618842c25428040fb7e1d168
Angellsworth <angellsworth@msn.com>
2025-03-19 20:49:16 -0600

    wip: fixed partials so the page would render

--- controllers/staticPages.js
+++ controllers/staticPages.js
@@ -8,13 +8,11 @@ router.get('/about', (req, res) => {
 
 // GET /info/contact Page
 router.get('/contact', (req, res) => {
-    console.log("✅ ✅PLEASE HIT ME!!!Contact page route hit!");
     res.render('pages/contact.ejs');
 });
 
 // GET /info/faq Page
 router.get('/faq', (req, res) => {
-    console.log("✅ FAQ page route hit!");
     res.render('pages/faq.ejs');
 });
 
